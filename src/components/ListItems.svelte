0f5b957c67597cbcb605270323465724af3c6c98
Aurélio Soares <95449811+aurelio-sc@users.noreply.github.com>
2022-03-05 18:09:01 -0300

    Quantity input

--- src/components/ListItems.svelte
+++ src/components/ListItems.svelte
@@ -1,25 +1,31 @@
 <script>
-  //adicionar um segundo input para a quantidade!
-  
+
+  let newQuantity = '';
   let newItem = '';
 
+  let quantityList = [];
   let todoList = [];
 
 function addToList() {
   todoList = [...todoList, {text: newItem, status: false}];
+  quantityList = [...quantityList, { text: newQuantity}]
   newItem = '';
+  newQuantity = '';
 }
 
 function removeFromList(index) {
   todoList.splice(index, 1)
+  quantityList.splice(index,1)
   todoList = todoList;
+  quantityList = quantityList;
   }
 </script>
 
-<input bind:value={newItem} type="text" placeholder="new todo item..">
-<button on:click={addToList}>Add</button>
+<input bind:value={newQuantity} type="text" placeholder="Quant.">
+<input bind:value={newItem} type="text" placeholder="Novo item">
+<button on:click={addToList}>Adicionar</button>
 
-<br/>
+<br/> <!--Fazer a quantidade aparecer!-->
 {#each todoList as item, index}
 <input bind:checked={item.status} type="checkbox">
 <span class:checked={item.status}>{item.text}</span>
