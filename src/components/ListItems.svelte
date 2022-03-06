6e681ee8b88b8e62267bfe63d412795af113038f
Aurélio Soares <95449811+aurelio-sc@users.noreply.github.com>
2022-03-06 10:51:46 -0300

    Quantity appears

--- src/components/ListItems.svelte
+++ src/components/ListItems.svelte
@@ -4,38 +4,68 @@
   let newItem = '';
 
   let quantityList = [];
-  let todoList = [];
+  let itemList = [];
 
 function addToList() {
-  todoList = [...todoList, {text: newItem, status: false}];
-  quantityList = [...quantityList, { text: newQuantity}]
+  itemList = [...itemList, {text: newItem}];
+  quantityList = [...quantityList, {number: newQuantity}]
   newItem = '';
   newQuantity = '';
-}
+};
 
 function removeFromList(index) {
-  todoList.splice(index, 1)
+  itemList.splice(index, 1)
   quantityList.splice(index,1)
-  todoList = todoList;
+  itemList = itemList;
   quantityList = quantityList;
-  }
+  };
+
 </script>
 
-<input bind:value={newQuantity} type="text" placeholder="Quant.">
-<input bind:value={newItem} type="text" placeholder="Novo item">
-<button on:click={addToList}>Adicionar</button>
+<div class="list">
 
-<br/> <!--Fazer a quantidade aparecer!-->
-{#each todoList as item, index}
-<input bind:checked={item.status} type="checkbox">
-<span class:checked={item.status}>{item.text}</span>
-<span on:click={() => removeFromList(index)}>❌</span>
-<br/>
-{/each} 
+  <div class="newListItem">
+    {#each quantityList as item, index}
+    <span class="quantity">{item.number}</span>
+    <br/>
+    {/each}
 
+
+    {#each itemList as item, index}
+
+    <span class:checked={item.status} class="item">{item.text}</span>
+    <span on:click={() => removeFromList(index)}>❌</span>
+    <br/>
+    {/each} 
+  </div>
+
+  <input bind:value={newQuantity} type="number" placeholder="Quant." class="quantity">
+  <input bind:value={newItem} type="text" placeholder="Novo item" class="item">
+  <button on:click={addToList}>Adicionar</button>
+
+  <br/>
+
+</div>
 
 <style> 
-.checked {
-      text-decoration: line-through;
-  }
-</style> 
+
+.newListItem{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+.quantity{
+  width: 5rem;
+}
+
+.item{
+  width: 10rem;
+  text-align: start;
+}
+</style>
+
+<!--Centralizar os items da lista
+    Alterar ícone de remover
+    Substituir o botão adicionar por uma imagem-->
